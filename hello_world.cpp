525616137a456efa681b04f81f97437405522628
saikiiri <saikiiri@vk.com>
2024-03-20 09:09:53 +0700

    added hello_world.cpp with name

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void)
+{
+    string Name;
+    cout << "Enter your name:" << "\t";
+    cin >> Name;
+    cout << "Hello world from @" << Name << endl;
+
+    return 0;
+}
