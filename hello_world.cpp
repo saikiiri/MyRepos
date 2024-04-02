6e86973776d8b4b719de1086d9e11c9ec7641435
saikiiri <saikiiri@vk.com>
2024-04-02 21:49:11 +0700

    Comment hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    std::string Imya;
+    std::string Imya; //Added string for name
     std::cout << "Enter your name:" << "\t";
     std::cin >> Imya;
     std::cout << "Hello world from @" << Imya << std::endl;
